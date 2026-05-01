83351855553dd60406e18268f3211a60bb7e4b69
harshad8623 <dhuppeh@gmail.com>
2026-05-01 22:58:47 +0530

    fix: deployment hardening - add pywebpush/vapid deps to requirements.txt, fix gunicorn/psycopg2 version pins, remove debug=True from run.py, add render.yaml with flask db upgrade build step

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from app.extensions import db
 
@@ -6,4 +7,5 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    debug = os.environ.get('FLASK_DEBUG', '0') == '1'
+    app.run(debug=debug, host='0.0.0.0', port=5001)
